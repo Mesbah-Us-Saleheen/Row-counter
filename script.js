f74a56b8e2dafe0a506be52772a857934353a1f6
Mesbah-Us-Saleheen <122684874+Mesbah-Us-Saleheen@users.noreply.github.com>
2023-01-16 23:09:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment(){
+    count += 1
+    countEl.innerHTML = count
+}
+
+function save(){
+    saveEl.innerHTML += count + " - "
+    count = 0
+    countEl.innerHTML = count
+    console.log(count)
+}
